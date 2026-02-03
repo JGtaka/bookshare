3c91721ddbed7aa41ab63b351a30bc94f3516401
JGtaka <JGtakahiro@outlook.jp>
2026-02-03 23:56:06 +0900

    Rspecを導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'is valid with email and password' do
+    user = User.new(
+      email: 'test@example.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+
+    expect(user).to be_valid
+  end
+end
