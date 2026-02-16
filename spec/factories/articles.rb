4a1c14ac4b3545118d89df3dba5a6b8c727dae7e
JGtaka <JGtakahiro@outlook.jp>
2026-02-16 20:02:52 +0900

    バリテーションテスト通過

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :article do
+    sequence(:title) { |n| "テスト記事#{n}" }
+    body { "これはテスト記事の内容です。" }
+    curriculum { "テスト記事のカリキュラムです。" }
+    association :user
   end
 end
