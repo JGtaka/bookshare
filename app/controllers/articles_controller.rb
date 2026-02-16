23af02aba82758e0369174be939622801afcf6b4
JGtaka <JGtakahiro@outlook.jp>
2026-02-16 16:26:48 +0900

    記事作成機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def new 
     @article = Article.new
   end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to articles_path, success: t('defaults.flash_message.created', item: Article.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_created', item: Article.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :curriculum)
+  end
 end
